b6d7232a76f5182276f054cef5946288d2be74a5
ryan <64014182@qq.com>
2018-05-23 16:49:30 +0800

    create

--- src/main/java/cn/com/free/ryan/shop/SellApplication.java
+++ src/main/java/cn/com/free/ryan/shop/SellApplication.java
@@ -2,7 +2,15 @@ package cn.com.free.ryan.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Description： TODO
+ * @author: qianchong
+ * Date: Created in 2018/5/23 16:49
+ * Company: senb
+ * Copyright: Copyright (c) 2018/5/23
+ * Version: 0.0.1
+ * Modified By: 
+ */
 @SpringBootApplication
 public class SellApplication {
 
